8d402deb777463a27fb3b82d80a12e01606e447d
Frida Rosenaa <macrosedk@gmail.com>
2026-02-18 00:32:42 +0100

    update pathing

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,17 @@ const config = {
   kit: {
     adapter: adapter({
       fallback: 'index.html',
+      // default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
     }),
+    paths: {
+      relative: false
+    }
   },
 };
 
